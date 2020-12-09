f285b855abc2ea66960a42ed927a175cea8e644c
kevin.song <songkang666@126.com>
2020-12-09 11:45:57 +0800

    更新版本到 1.6.14
    ChangeLog
    1. 修复 iOS 及 macOS 下无法正常播放的问题

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -295,7 +295,7 @@ declare module '__urtc-sdk/resolutions' {
 }
 
 declare module '__urtc-sdk/version' {
-  export const version = "1.6.13";
+  export const version = "1.6.14";
 }
 
 declare module '__urtc-sdk/token' {
