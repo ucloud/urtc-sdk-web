0ee46c235c94e757547f3331ff7a04d02bf01588
kevin.song <songkang666@126.com>
2020-12-15 20:21:02 +0800

    更新版本到 1.6.16
    ChangeLog
    1. 修复 mute video 时，play 成功但回调函数未触发的问题

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -295,7 +295,7 @@ declare module '__urtc-sdk/resolutions' {
 }
 
 declare module '__urtc-sdk/version' {
-  export const version = "1.6.15";
+  export const version = "1.6.16";
 }
 
 declare module '__urtc-sdk/token' {
