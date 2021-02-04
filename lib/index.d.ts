bf7454cb894a3f85ea83868c8a0cbe5e1912e6a1
kevin.song <songkang666@126.com>
2021-02-04 13:21:56 +0800

    更新版本到 1.6.20
    ChangeLog
    1. 新增 player-status-change 事件，用于通知播放器状态的变化
    2. 修复无音频时，iOS 微信浏览器无法显示控制面板的问题
    3. 修复断网时有机率出现本地流黑屏的问题
    4. 修复 iOS 微信浏览器下，静音导致视频播放失败的问题
    5. 其他内部优化

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -298,7 +298,7 @@ declare module '__urtc-sdk/resolutions' {
 }
 
 declare module '__urtc-sdk/version' {
-  export const version = "1.6.19";
+  export const version = "1.6.20";
 }
 
 declare module '__urtc-sdk/token' {
@@ -319,7 +319,7 @@ declare module '__urtc-sdk/types' {
   export type RoomType = 'rtc' | 'live';
   export type UserRole = 'pull' | 'push' | 'push-and-pull';
   export type DeviceType = 'audio' | 'video';
-  export type EventType = 'user-added' | 'user-removed' | 'stream-added' | 'stream-removed' | 'stream-published' | 'stream-subscribed' | 'mute-video' | 'unmute-video' | 'mute-audio' | 'unmute-audio' | 'screenshare-stopped' | 'connection-state-change' | 'kick-off' | 'network-quality' | 'stream-reconnected' | 'record-notify' | 'relay-notify' | 'volume-indicator' | 'error-notify' | 'stream-playing' | 'stream-paused';
+  export type EventType = 'user-added' | 'user-removed' | 'stream-added' | 'stream-removed' | 'stream-published' | 'stream-subscribed' | 'mute-video' | 'unmute-video' | 'mute-audio' | 'unmute-audio' | 'screenshare-stopped' | 'connection-state-change' | 'kick-off' | 'network-quality' | 'stream-reconnected' | 'record-notify' | 'relay-notify' | 'volume-indicator' | 'error-notify' | 'stream-playing' | 'stream-paused' | 'player-status-change';
   export type ConnectionState = 'OPEN' | 'CONNECTING' | 'CLOSING' | 'RECONNECTING' | 'CLOSED';
   export type WaterMarkPosition = 'left-top' | 'left-bottom' | 'right-top' | 'right-bottom';
   export type WaterMarkType = 'time' | 'image' | 'text';
@@ -622,6 +622,11 @@ declare module '__urtc-sdk/types' {
     uid: string;
     mediaType: 'camera' | 'screen';
     volume: number;
+  }
+  export interface PlayerStatusEvent {
+    type: 'audio' | 'video';
+    status: 'playing' | 'paused';
+    stream: Stream;
   }
 }
 
